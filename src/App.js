f80b2bdaa6a7d184e3dc9ecf1e7107b9f6eb0e13
rajesh ks <rajeshks2566@gmail.com>
2022-04-21 15:47:55 +0530

    React-Spotify-Login

--- src/App.js
+++ src/App.js
@@ -5,16 +5,14 @@ import './global.css'
 import EmpForm from "./employee/EmpForm";
 import Products from "./project/Products";
 import SpotifyLogin from "./spotify/SpotifyLogin";
-import ToDoList from "./todolist/ToDoList";
 
 function App() {
   return <div>
     {/* <Navbar /> */}
     {/* <EmpForm /> */}
     {/* <Products /> */}
-    {/* <SpotifyLogin /> */}
-    <ToDoList />
-  </div>;
+    <SpotifyLogin />
+ </div>;
 }
 
 export default App;
